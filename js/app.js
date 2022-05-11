815fd0eed4bf14eb47453b44ca669f01c3eaa2dc
Your Name <paulo-cesar-batista@hotmail.com>
2022-05-11 09:53:31 +0100

    captacao e verificacao de dados ativo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,70 @@
+class Despesa {
+
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor; 
+    }
+
+    validarDados(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i] == null){
+                return false;
+            }   
+        }
+                return true;
+    }
+}
+
+class BancoDados{
+
+    constructor(){
+        let id = localStorage.getItem('id')
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1;
+    }
+    gravar(d) {
+        //local sotorage permite adicionar o conteudo a pagina e armazenar;
+        //metodo setItem substitui o registro anterior ao inves de adicionar um novo;
+        
+        let id = this.getProximoId();
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+        
+    }
+}
+
+let bancoDados = new BancoDados()
 
 function cadastrarDespesas() {
+
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
     let dia = document.getElementById('dia');
     let descricao = document.getElementById('descricao');
     let valor = document.getElementById('valor');
 
-    console.log(ano.value);
-    console.log(mes.value);
-    console.log(dia.value);
-    console.log(descricao.value);
-    console.log(valor.value);
+    let despesa = new Despesa(ano.value, mes.value, dia.value, descricao.value, valor.value);
+    console.log(despesa);
 
+
+
+    if(despesa.validarDados()){
+        console.log('Dados Validos');
+    }else{
+        console.log('Dados invalidos')
+    }
 }
+
+
 
 
